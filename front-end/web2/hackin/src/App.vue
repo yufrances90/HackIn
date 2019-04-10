b00f54d01d9a2c37bb41964d54be66db695613b0
yufrances90 <yufrances90@gmail.com>
2019-04-10 12:28:19 -0700

    feat: Built customized navigation bar

--- front-end/web2/hackin/src/App.vue
+++ front-end/web2/hackin/src/App.vue
@@ -1,19 +1,31 @@
 <template>
     <div id="app">
-        <p>
-            Hello World
-        </p>
+        <c-navbar></c-navbar>
     </div>
 </template>
 
 <script>
 
-export default {
-    name: 'app',
-    components: {
+    import CNavbar from './components/CNavbar.vue';
+
+    export default {
+        name: 'app',
+        components: {
+            CNavbar
+        }
     }
-}
 </script>
 
-<style>
+<style lang="scss">
+
+    @import "~vue-material/dist/theme/engine"; // Import the theme engine
+
+    @include md-register-theme("default", (
+        primary: md-get-palette-color(black, A200), // The primary color of your application
+        accent: md-get-palette-color(red, A200), // The accent or secondary color
+        theme: light // This can be dark or light
+    ));
+
+    @import "~vue-material/dist/theme/all"; // Apply the theme
+
 </style>
---
+++ front-end/web2/hackin/src/components/CNavbar.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="page-container">
+        <MdToolbar class="md-primary md-medium">
+            <h3 class="md-title" style="flex: 1">
+                {{ title }}
+            </h3>
+        </MdToolbar>
+    </div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+import { 
+    MdToolbar,
+    MdButton 
+} from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(MdToolbar);
+Vue.use(MdButton);
+
+export default {
+  name: 'CNavbar',
+  data: () => ({
+      title: "HackMMunity"
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+  .md-app {
+    max-height: 400px;
+    border: 1px solid rgba(#000, .12);
+  }
+
+  .md-app-toolbar {
+    height: 196px;
+  }
+
+   // Demo purposes only
+  .md-drawer {
+    width: 230px;
+    max-width: calc(100vw - 125px);
+  }
+</style>
+
